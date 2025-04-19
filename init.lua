b3df474fd2803e2681b42ed1ae01a0078856e6da
Ates <88388753+AtesIsf@users.noreply.github.com>
2025-04-19 17:55:43 -0400

    removed tab entirely

--- init.lua
+++ init.lua
@@ -146,29 +146,6 @@ cmp.setup({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
-    ['<Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      else
-        -- Fall back to normal indentation
-        vim.api.nvim_feedkeys(
-          vim.api.nvim_replace_termcodes('<Tab>', true, true, true),
-          'n',
-          false
-        )
-      end
-    end, { 'i', 's' }),
-    ['<S-Tab>'] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_prev_item()
-      elseif luasnip.jumpable(-1) then
-        luasnip.jump(-1)
-      else
-        fallback()
-      end
-    end, { 'i', 's' }),
   }),
   sources = {
     { name = 'nvim_lsp' },
