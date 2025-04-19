1fcec495e8ccc2b39a9a205b08114b7abb928e08
Ates <88388753+AtesIsf@users.noreply.github.com>
2025-04-19 18:00:03 -0400

    removed luasnip tab functionality

--- init.lua
+++ init.lua
@@ -146,6 +146,13 @@ cmp.setup({
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     }),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
   }),
   sources = {
     { name = 'nvim_lsp' },
