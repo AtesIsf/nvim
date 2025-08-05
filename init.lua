b138b86497f60e5645f10d147ec8af38a68f9493
Ates <88388753+AtesIsf@users.noreply.github.com>
2025-08-05 21:36:03 +0300

    solved autocomplete issue

--- init.lua
+++ init.lua
@@ -91,7 +91,6 @@ vim.cmd [[
 -- LSP Client stuff
 local on_attach = function(client, bufnr)
   local opts = { buffer = bufnr, silent = true }
-  client.server_capabilities.completionProvider = nil
 
   -- Keybindings
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
